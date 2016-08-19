d5e6913f031a50ea103fe093e29c4db2c3996cab
JakO <jk.sbrn@gmail.com>
2016-08-19 13:47:14 +0100

    remove index jade. fix sticky header and polyfill

--- app/scripts/controllers/challenge.js
+++ app/scripts/controllers/challenge.js
@@ -97,6 +97,12 @@ angular.module('stageBuilderApp')
             ctrl.inspectorTabActive = 3;
             ctrl.selected = stage;
         };
+
+        var stickyElements = document.getElementsByClassName('sticky');
+
+        for (var i = stickyElements.length - 1; i >= 0; i--) {
+            Stickyfill.add(stickyElements[i]);
+        }
     }
 })
 .component('stageBuilderMenu', {
